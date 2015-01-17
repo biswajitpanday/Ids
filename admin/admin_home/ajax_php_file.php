80d2566bceead71cfbb6db9f83c5eef9d0d23c8a
biswajitpanday <biswajitmailid@gmail.com>
2015-01-18 04:48:27 +0600

    Code refactor & directory create for products.

--- admin/admin_home/ajax_php_file.php
+++ admin/admin_home/ajax_php_file.php
@@ -16,17 +16,21 @@
 				echo "Return Code: " . $_FILES["file"]["error"] . "<br/><br/>";
 			}
 			else {
-				if (file_exists("../../upload/" . $_FILES["file"]["name"])) {
-					echo $_FILES["file"]["name"] . " <span id='invalid'><b>already exists.</b></span> ";
+				if (file_exists("../../upload/". $productType . "/" . $_FILES["file"]["name"])) {
+					//echo $productType . "/" . $_FILES["file"]["name"] . "/" .  " <span id='invalid'><b>already exists.</b></span> ";
+					echo " <span id='invalid'><b>Product already exists.</b></span> ";
 				}
 				else {
-
+					if(!is_dir("../../upload/" . $productType)){
+						mkdir("../../upload/" . $productType);
+					}
 					$sourcePath = $_FILES['file']['tmp_name']; // Storing source path of the file in a variable
-					$targetPath = "../../upload/".$_FILES['file']['name']; // Target path where file is to be stored
+					$targetPath = "../../upload/" . $productType . "/" .$_FILES['file']['name']; // Target path where file is to be stored
 					$move_success = move_uploaded_file($sourcePath,$targetPath) ; // Moving Uploaded file
 
 					if(isset($productName) && isset($productType) && isset($productPrice) && isset($productQuantity) && isset($productFeature) && isset($move_success)) {
-						$image_path_with_file_name = substr($targetPath, 5);
+						$image_name = substr($targetPath, 13);
+						$image_path_with_file_name = "/upload/" . $image_name; 
 
 						$prouucts = array($productName, $productType, $productPrice, $productQuantity, $productFeature, $image_path_with_file_name);
 						include 'connection_query.php';
